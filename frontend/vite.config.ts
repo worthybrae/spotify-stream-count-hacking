2c69370f44305ba7677e6d8e09ef133c62cef561
worthybrae <worthyrae@gmail.com>
2025-02-24 14:51:37 -0500

    updating vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
-import path from "path"
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
